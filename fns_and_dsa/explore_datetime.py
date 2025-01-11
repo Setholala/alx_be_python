87167db58086adc601185691b94991cf08d3ec12
seth <sethedi40@gmail.com>
2025-01-10 21:06:45 -0800

    Correct format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     current_date = datetime.now()
     formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print(formatted_date)
+    print(f"Current date and time: {formatted_date}")
 
 
 
@@ -11,12 +11,12 @@ def calculate_future_date(number_of_days):
     current_date = datetime.now()
     future_date = timedelta(days=number_of_days) + current_date
     formatted_date = future_date.strftime("%Y-%m-%d")
-    print(formatted_date)
+    print(f"Future date: {formatted_date}")
 
     
 display_current_datetime()
 
-number_of_days = int(input("Enter number of days: "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 calculate_future_date(number_of_days)
 
