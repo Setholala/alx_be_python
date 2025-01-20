9363b0d34e39caaceaeedfe7d5943912ba317d34
seth <sethedi40@gmail.com>
2025-01-20 04:29:40 -0800

    Track book in a library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,32 @@
 class Book:
-    def __init__(self, title, author, _is_checked_out):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = _is_checked_out
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self._books = list()
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
+                print(f"{book.title} has been checked out")
+            else:
+                print(f"{book.title} is not currently available")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book._is_checked_out:
+                book._is_checked_out = False
+                print(f"{book.title} has been returned")
+                
+
+    def list_available_books(self):
+        for book in self._books:
+            if not book._is_checked_out:
+                print(book.title)
