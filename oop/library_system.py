7788c7f0b90bba47f4c895db0a36da72b354170f
seth <sethedi40@gmail.com>
2025-01-26 11:44:58 -0800

    Correct syntax

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,25 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __str__(self):
+         return f"{self.__class__.__name__}: {self.title}"
         
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+         return f"{self.__class__.__name__}: {self.title}, {self.file_size}"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.title}, {self.page_count}"
 
 
 class Library:
@@ -23,12 +32,5 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(f"{book.__class__.__name__}: {book.title}", end="")
-
-            if isinstance(book, EBook):
-                print(f", {book.file_size}")
-            elif isinstance(book, PrintBook):
-                print(f", {book.page_count}")
-            else:
-                print()
+            print(book) 
     
